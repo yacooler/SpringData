cbae8e9b9fccd00785f0a8ca6ba0c6453f0a0649
yacooler <yacooler@yandex.ru>
2020-12-24 20:05:52 +0300

    Initial commit

---
+++ src/test/java/ru/vyazankin/SpringData/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vyazankin.SpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
